e473187826d246c24e1500ca5457beb6779f5450
AlvaroChambi <alvaro.chambi.campos@gmail.com>
2020-01-18 20:09:38 +0100

    Moved logic to util

--- app/src/main/java/es/developers/achambi/afines/profile/presenter/ProfilePresenter.kt
+++ app/src/main/java/es/developers/achambi/afines/profile/presenter/ProfilePresenter.kt
@@ -10,6 +10,7 @@ import es.developers.achambi.afines.profile.ui.ProfileScreenInterface
 import es.developers.achambi.afines.profile.ui.presentations.ProfilePresentationBuilder
 import es.developers.achambi.afines.profile.usecase.ProfileUseCase
 import es.developers.achambi.afines.repositories.model.FirebaseProfile
+import es.developers.achambi.afines.utils.IBANUtil
 
 class ProfilePresenter(screen: ProfileScreenInterface,
                        lifecycle: Lifecycle,
@@ -45,35 +46,11 @@ class ProfilePresenter(screen: ProfileScreenInterface,
     }
 
     fun validateIban(iban: String) {
-        if(isIbanValid(iban)) {
+        if(IBANUtil.isIbanValid(iban)) {
             screen.showIbanValidated()
         } else {
             screen.showIbanRejected()
         }
-    }
-
-    private fun isIbanValid(iban: String): Boolean {
-        val IBAN_MIN_SIZE = 15
-        val IBAN_MAX_SIZE = 34
-        val IBAN_MAX: Long = 999999999
-        val IBAN_MODULUS: Long = 97
-        val trimmed = iban.trim { it <= ' ' }
-        if (trimmed.length < IBAN_MIN_SIZE || trimmed.length > IBAN_MAX_SIZE) {
-            return false
-        }
-        val reformat = trimmed.substring(4) + trimmed.substring(0, 4)
-        var total: Long = 0
-        for (i in 0 until reformat.length) {
-            val charValue = Character.getNumericValue(reformat[i])
-            if (charValue < 0 || charValue > 35) {
-                return false
-            }
-            total = (if (charValue > 9) total * 100 else total * 10) + charValue
-            if (total > IBAN_MAX) {
-                total = total % IBAN_MODULUS
-            }
-        }
-        return total % IBAN_MODULUS == 1L
     }
 
     fun logout() {
