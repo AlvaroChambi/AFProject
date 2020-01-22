b0f70d69f8e18a1083792f9f5833e417384a91c6
achambi <alvaro.chambi.campos@gmail.com>
2020-01-22 18:39:29 +0100

    Removed unused file

--- app/src/main/java/es/developers/achambi/afines/invoices/ui/UploadScreenInterface.kt
+++
@@ -1,7 +0,0 @@
-package es.developers.achambi.afines.invoices.ui
-
-import android.net.Uri
-import es.developer.achambi.coreframework.ui.Screen
-import es.developers.achambi.afines.invoices.model.Invoice
-import es.developers.achambi.afines.invoices.model.InvoiceUpload
-
